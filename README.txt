8ea8598146cf8bd22e5e3b1e546ba87e9779f05c
Mat <mat.gregory@virginmedia.com>
2014-06-21 06:35:52 +0100

    Desert road climbs hills, places halfslabs. Remove tunnels

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-path 0.1.6 by paramat
+path 0.2.0 by paramat
 For latest stable Minetest back to 0.4.8
 Depends default
 Licenses: code WTFPL
--- init.lua
+++ init.lua
@@ -1,34 +1,27 @@
--- path 0.1.6 by paramat
+-- path 0.2.0 by paramat
 -- For latest stable Minetest and back to 0.4.8
 -- Depends default
 -- License: code WTFPL
 
--- nodeupdate(pos) for light spread
--- 2 lines of lights
--- add desert stone, red concrete
-
 -- Parameters
 
-local YFLAT = 1 -- Flat area elevation
-local TERSCA = 64 -- Vertical terrain scale
-local TFLAT = 0.2 -- Flat area width
-local WID = 3 -- Road lane width
+local TERSCA = 32 -- Vertical terrain scale
 local CACCHA = 1 / 192 ^ 2 -- Cactus chance per node
 
--- 2D noise for path and terrain
+-- 2D noise for terrain
 
-local np_base = {
+local np_terrain = {
 	offset = 0,
 	scale = 1,
-	spread = {x=1024, y=1024, z=1024},
+	spread = {x=512, y=512, z=512},
 	seed = -9111,
 	octaves = 4,
 	persist = 0.4
 }
 
--- 2D noise for alt path and tunnels
+-- 2D noise for path
 
-local np_alt = {
+local np_path = {
 	offset = 0,
 	scale = 1,
 	spread = {x=1024, y=1024, z=1024},
@@ -41,7 +34,27 @@ local np_alt = {
 
 minetest.register_node("path:roadblack", {
 	description = "Road Black",
-	tiles = {"path_roadblack.png"},
+	tiles = {"default_obsidian.png"},
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("path:roadslab", {
+	description = "Road Slab",
+	tiles = {"default_obsidian.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	sunlight_propagates = true,
+	buildable_to = false,
+	node_box = {
+		type = "fixed",
+		fixed = {{-0.5, -0.5, -0.5, 0.5, 0, 0.5}},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {{-0.5, -0.5, -0.5, 0.5, 0, 0.5}},
+	},
 	groups = {cracky=2},
 	sounds = default.node_sound_stone_defaults(),
 })
@@ -51,22 +64,6 @@ minetest.register_node("path:roadwhite", {
 	tiles = {"path_roadwhite.png"},
 	groups = {cracky=2},
 	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("path:lightoff", {
-	description = "Light",
-	tiles = {"path_light.png"},
-	light_source = 12,
-	groups = {cracky=3,oddly_breakable_by_hand=3},
-	sounds = default.node_sound_glass_defaults(),
-})
-
-minetest.register_node("path:lighton", {
-	description = "Light",
-	tiles = {"path_light.png"},
-	light_source = 12,
-	groups = {cracky=3,oddly_breakable_by_hand=3},
-	sounds = default.node_sound_glass_defaults(),
 })
 
 minetest.register_node("path:cactus", {
@@ -80,29 +77,20 @@ minetest.register_node("path:cactus", {
 	on_place = minetest.rotate_node
 })
 
-minetest.register_node("path:redcrete", {
-	description = "Red Concrete",
-	tiles = {"path_redcrete.png"},
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults(),
-})
-
 -- Stuff
 
 path = {}
 
-local rad = WID ^ 2 + 4
-
 -- Set mapgen parameters
 
 minetest.register_on_mapgen_init(function(mgparams)
-	minetest.set_mapgen_params({mgname="singlenode"})
+	minetest.set_mapgen_params({mgname="singlenode", water_level=-32})
 end)
 
 -- Spawn player
 
 function spawnplayer(player)
-	player:setpos({x=0, y=2, z=0})
+	player:setpos({x=0, y=48, z=0})
 end
 
 minetest.register_on_newplayer(function(player)
@@ -128,18 +116,6 @@ function path_cactus(x, y, z, area, data)
 	end
 end
 
--- ABM
-
-minetest.register_abm({
-	nodenames = {"path:lightoff"},
-	interval = 5,
-	chance = 8,
-	action = function(pos, node)
-		minetest.add_node(pos, {name="path:lighton"})
-		nodeupdate(pos)
-	end,
-})
-
 -- On generated function
 
 minetest.register_on_generated(function(minp, maxp, seed)
@@ -162,103 +138,93 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local data = vm:get_data()
 	
 	local c_air = minetest.get_content_id("air")
+	local c_ignore = minetest.get_content_id("ignore")
 	local c_desand = minetest.get_content_id("default:desert_sand")
 	local c_destone = minetest.get_content_id("default:desert_stone")
-	local c_redcrete = minetest.get_content_id("path:redcrete")
 	local c_roadblack = minetest.get_content_id("path:roadblack")
+	local c_roadslab = minetest.get_content_id("path:roadslab")
 	local c_roadwhite = minetest.get_content_id("path:roadwhite")
-	local c_light = minetest.get_content_id("path:lightoff")
 	
 	local sidelen = x1 - x0 + 1
 	local chulens = {x=sidelen+1, y=sidelen+1, z=sidelen} -- x = coord x, y = coord z
 	local minposxz = {x=x0-1, y=z0-1}
 	
-	local nvals_base = minetest.get_perlin_map(np_base, chulens):get2dMap_flat(minposxz)
-	local nvals_alt = minetest.get_perlin_map(np_alt, chulens):get2dMap_flat(minposxz)
+	local nvals_terrain = minetest.get_perlin_map(np_terrain, chulens):get2dMap_flat(minposxz)
+	local nvals_path = minetest.get_perlin_map(np_path, chulens):get2dMap_flat(minposxz)
 	
 	local nixz = 1
 	for z = z0-1, z1 do
 		for y = y0, y1 do
 			local vi = area:index(x0-1, y, z)
-			local n_xprebase = false
-			local n_xprealt = false
+			local viu = area:index(x0-1, y-1, z)
+			local n_xprepath = false
 			for x = x0-1, x1 do
 				local nodid = data[vi]
-				local ysurf
-				local flat = false
-				local n_zprebase = nvals_base[(nixz - 81)]
-				local n_zprealt = nvals_alt[(nixz - 81)]
+				local nodidu = data[viu]
+				local n_zprepath = nvals_path[(nixz - 81)]
 				local chunk = (x >= x0 and z >= z0)
 				
-				local n_alt = nvals_alt[nixz]
-				local n_absalt = math.abs(n_alt)
-				local n_base = nvals_base[nixz]
-				local n_absbase = math.abs(n_base)
-				if n_absbase > TFLAT then
-					ysurf = YFLAT + math.floor((n_absbase - TFLAT) * TERSCA)
-				else
-					ysurf = YFLAT
-					flat = true
-				end
+				local n_path = nvals_path[nixz]
+				local n_abspath = math.abs(n_path)
+				local n_terrain = nvals_terrain[nixz]
+				local ysurf = math.floor(n_terrain * TERSCA)
 				
 				if chunk then
-					if y == YFLAT
-					and (((n_alt >= 0 and n_xprealt < 0)
-					or (n_alt < 0 and n_xprealt >= 0))
-					or ((n_alt >= 0 and n_zprealt < 0)
-					or (n_alt < 0 and n_zprealt >= 0))) then
-						data[vi] = c_roadwhite
-						for i = -WID, WID do
-						for k = -WID, WID do
-							if (math.abs(i)) ^ 2 + (math.abs(k)) ^ 2 <= rad then
-								local vi = area:index(x+i, y, z+k)
-								local nodid = data[vi]
-								if nodid ~= c_roadwhite then
-									data[vi] = c_roadblack
-								end
-							end
-						end
-						end
-					elseif y >= YFLAT + 1 and y <= YFLAT + 4 and n_absalt < 0.025 then
-						data[vi] = c_air
-					elseif y <= ysurf - 1 and y == YFLAT + 5 and n_absalt > 0.003 and n_absalt < 0.007 then
-						data[vi] = c_light
-					elseif y <= ysurf and y >= YFLAT and y <= YFLAT + 5
-					and n_absalt < 0.035 and n_absbase > TFLAT and nodid ~= c_roadblack then
-						data[vi] = c_redcrete
-					elseif y == ysurf then
-						if ((n_base >= 0 and n_xprebase < 0)
-						or (n_base < 0 and n_xprebase >= 0))
-						or ((n_base >= 0 and n_zprebase < 0)
-						or (n_base < 0 and n_zprebase >= 0)) then
+					if y == ysurf then
+						if ((n_path >= 0 and n_xprepath < 0)
+						or (n_path < 0 and n_xprepath >= 0))
+						or ((n_path >= 0 and n_zprepath < 0)
+						or (n_path < 0 and n_zprepath >= 0)) then
 							data[vi] = c_roadwhite
-							for i = -WID, WID do
-							for k = -WID, WID do
-								if (math.abs(i)) ^ 2 + (math.abs(k)) ^ 2 <= rad then
+							for i = -4, 4 do
+							for k = -4, 4 do
+								local radsq = (math.abs(i)) ^ 2 + (math.abs(k)) ^ 2
+								if radsq <= 13 then
 									local vi = area:index(x+i, y, z+k)
+									local via = area:index(x+i, y+1, z+k)
 									local nodid = data[vi]
+									local nodida = data[via]
 									if nodid ~= c_roadwhite then
 										data[vi] = c_roadblack
+										if nodida == c_desand then
+											data[via] = c_air
+										end
+									end
+								elseif radsq <= 20 then
+									local vi = area:index(x+i, y, z+k)
+									local via = area:index(x+i, y+1, z+k)
+									local nodid = data[vi]
+									local nodida = data[via]
+									if nodid ~= c_roadblack
+									and nodid ~= c_roadwhite
+									and nodid ~= c_desand then
+										data[vi] = c_roadslab
+										if nodida == c_desand then
+											data[via] = c_air
+										end
 									end
 								end
 							end
 							end
-						elseif nodid ~= c_roadblack and nodid ~= c_roadwhite then
+						elseif nodid ~= c_roadblack
+						and nodid ~= c_roadwhite
+						and nodidu ~= c_roadblack
+						and nodidu ~= c_roadslab then
 							data[vi] = c_desand
 							if math.random() < CACCHA then
 								path_cactus(x, y+1, z, area, data)
 							end
 						end
-					elseif y <= ysurf - 8 and nodid ~= c_roadblack then
+					elseif y <= ysurf - 8 then
 						data[vi] = c_destone
 					elseif y < ysurf and nodid ~= c_roadblack then
 						data[vi] = c_desand
 					end
 				end
-				n_xprebase = n_base
-				n_xprealt = n_alt
+				n_xprepath = n_path
 				nixz = nixz + 1
 				vi = vi + 1
+				viu = viu + 1
 			end
 			nixz = nixz - 81
 		end
